bbde24e016c4c16e0fe16612cf79cb041e2542d0
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-03-12 23:07:53 +0100

    Constructor and methods of class History created

--- src/history/History.java
+++ src/history/History.java
@@ -4,13 +4,31 @@ import messages.Ack;
 
 import java.util.List;
 
-public class History {
+public class History
+{
     List<Ack> historyList;
 
+    public History()
+    {
+        historyList = new ArrayList<Ack>();
+    }
+
+    /**
+     * adding new ACK to list - history
+     * @param obj
+     * @return true if operation succeeded
+     */
     public boolean add(Ack obj)
     {
-        return false;
+        return historyList.add(obj);
     }
+
+    /**
+     * return list of ACKs
+     *
+     * TODO return non-editable version of list
+     * @return list of ACKs
+     */
     public List<Ack> returnList()
     {
         return historyList;
