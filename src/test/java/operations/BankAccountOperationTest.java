5e3e53cda09dd290e1d5408935182ccdcb09cf77
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-03-16 12:43:17 +0100

    First test for creating new normalAccount

---
+++ src/test/java/operations/BankAccountOperationTest.java
@@ -0,0 +1,26 @@
+package operations;
+
+import history.History;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import services.BankAccount;
+
+public class BankAccountOperationTest
+{
+    private History history;
+
+    @Before
+    public void setUp()
+    {
+        history = new History();
+    }
+
+    @Test
+    public void testCreatingAccount()
+    {
+        BankAccount account = BankAccountOperation.createNormalAccount(0, 10, history, "test");
+        BankAccount normalAccount = new BankAccount(0, 10);
+        Assert.assertEquals(normalAccount, account);
+    }
+}
